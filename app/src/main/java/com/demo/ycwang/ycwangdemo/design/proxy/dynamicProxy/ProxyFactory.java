d0f23f8b319ce688d4981dc0316ea27d9fea5a8e
ycwang <731651897@qq.com>
2019-01-07 17:53:14 +0800

    data

---
+++ app/src/main/java/com/demo/ycwang/ycwangdemo/design/proxy/dynamicProxy/ProxyFactory.java
@@ -0,0 +1,34 @@
+package com.demo.ycwang.ycwangdemo.design.proxy.dynamicProxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @Author: ycwang
+ * @Date: 2019-1-3 15:16
+ */
+public class ProxyFactory {
+
+    private Object target;
+
+    public ProxyFactory(Object target) {
+        this.target = target;
+    }
+
+
+    public Object getProxyInstance() {
+        return Proxy.newProxyInstance(target.getClass().getClassLoader(),
+                target.getClass().getInterfaces(),
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println("Begin Transaction");
+                        Object returnValue = method.invoke(target, args);
+                        System.out.println("Commit Transaction");
+                        return returnValue;
+                    }
+                });
+    }
+
+}
---
+++ app/src/main/java/com/demo/ycwang/ycwangdemo/design/proxy/staticProxy/Test.java
@@ -0,0 +1,14 @@
+package com.demo.ycwang.ycwangdemo.design.proxy.staticProxy;
+
+/**
+ * @Author: ycwang
+ * @Date: 2019-1-3 15:30
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        ProxyObject proxyObject = new ProxyObject(new RealObject());
+        proxyObject.operation();
+    }
+
+}
