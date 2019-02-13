1bc18abad10baa5b9bf1b48b589fc66beb9ddc06
ycwang <731651897@qq.com>
2019-02-13 09:43:20 +0800

    bitmap

---
+++ app/src/main/java/com/demo/ycwang/ycwangdemo/bitmap/BitmapUtil.java
@@ -0,0 +1,58 @@
+package com.demo.ycwang.ycwangdemo.bitmap;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+/**
+ * @Author: ycwang
+ * @Date: 2019/2/12 17:03
+ * @Description:
+ */
+public class BitmapUtil {
+
+
+    /**
+     * 获取合适大小的Bitmap图片
+     * @param res
+     * @param resId
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public static Bitmap decodeInSampleSize(Resources res, int resId, int reqWidth, int reqHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+
+    /**
+     * 计算Bitmap图片缩放比例
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        int height = options.outHeight;
+        int width = options.outWidth;
+        int sampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            int halfHeight = height / 2;
+            int halfWidth = width / 2;
+
+            while ((halfHeight / sampleSize) >= reqHeight
+                    && (halfWidth / sampleSize) >= reqWidth) {
+                sampleSize *= 2;
+            }
+        }
+        return sampleSize;
+    }
+
+}
