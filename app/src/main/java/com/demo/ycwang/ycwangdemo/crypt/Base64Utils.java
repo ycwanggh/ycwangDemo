c2a2d463ccebf38cb6b8fa1334682344d366feb7
ycwang <731651897@qq.com>
2019-03-18 10:01:48 +0800

    crypt

---
+++ app/src/main/java/com/demo/ycwang/ycwangdemo/crypt/Base64Utils.java
@@ -0,0 +1,88 @@
+package com.demo.ycwang.ycwangdemo.crypt;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.text.TextUtils;
+import android.util.Base64;
+
+import java.nio.charset.Charset;
+
+/**
+ * Base64工具类
+ *
+ * @author lijianqing
+ */
+
+public final class Base64Utils {
+
+    private Base64Utils() {
+
+    }
+
+    /**
+     * 标准的base64编码方式
+     */
+    public static String encodeDefault(final byte[] input) {
+        if (input == null) {
+            return null;
+        } else {
+            return new String(Base64.encode(input, Base64.DEFAULT), Charset.defaultCharset());
+        }
+    }
+
+    /**
+     * 去掉换行的base64编码方式
+     */
+    public static String encodeNoWrap(final byte[] input) {
+        if (input == null) {
+            return null;
+        } else {
+            return new String(Base64.encode(input, Base64.NO_WRAP), Charset.defaultCharset());
+        }
+    }
+
+    /**
+     * 去掉\n、去掉 = 、替换 + / 的base64编码方式
+     */
+    public static String encodeUrlParams(final byte[] input) {
+        if (input == null) {
+            return null;
+        } else {
+            return new String(Base64.encode(input, Base64.URL_SAFE | Base64.NO_PADDING | Base64.NO_WRAP), Charset.defaultCharset());
+        }
+    }
+
+    /**
+     * 标准的base64解码方式
+     */
+    public static byte[] decodeDefault(final String input) {
+        if (TextUtils.isEmpty(input)) {
+            return null;
+        } else {
+            return Base64.decode(input, Base64.DEFAULT);
+        }
+    }
+
+    /**
+     * 去掉换行的base64解码方式
+     */
+    public static byte[] decodeNowrap(final String input) {
+        if (TextUtils.isEmpty(input)) {
+            return null;
+        } else {
+            return Base64.decode(input, Base64.NO_WRAP);
+        }
+    }
+
+    /**
+     * 去掉\n、去掉 = 、替换 + / 的base64解码方式
+     */
+    public static byte[] decodeUrlParams(final String input) {
+        if (TextUtils.isEmpty(input)) {
+            return null;
+        } else {
+            return Base64.decode(input, Base64.URL_SAFE | Base64.NO_PADDING | Base64.NO_WRAP);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/demo/ycwang/ycwangdemo/crypt/CryptMD5.java
@@ -0,0 +1,117 @@
+package com.demo.ycwang.ycwangdemo.crypt;
+
+import android.text.TextUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @Description:
+ * @Author: ycwang
+ * @Date: 2019/3/15 14:52
+ */
+public class CryptMD5 {
+
+
+    /**
+     * MD5加盐
+     *
+     * @param string 字符串
+     * @param slat   盐
+     * @return
+     */
+    public static byte[] generateMD5(String string, String slat) {
+        try {
+            if (TextUtils.isEmpty(string)) {
+                throw new IllegalArgumentException();
+            } else {
+                MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+                messageDigest.update((slat + string).getBytes("UTF-8"));
+                return messageDigest.digest();
+            }
+        } catch (IllegalArgumentException | NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+
+    /**
+     * 对字符串多次md5加密
+     *
+     * @param string 字符串
+     * @param times  md5次数
+     * @return
+     */
+    public static byte[] generateMD5(String string, int times) {
+        if (TextUtils.isEmpty(string)) {
+            throw new IllegalArgumentException();
+        }
+        byte[] md5 = generateMD5(string);
+        for (int i = 0; i < times - 1; i++) {
+            md5 = generateMD5(new String(md5));
+        }
+        return generateMD5(new String(md5));
+    }
+
+
+    /**
+     * 生成给定字符串的MD5值
+     *
+     * @param input 字符串
+     * @return
+     */
+    public static byte[] generateMD5(final String input) {
+        try {
+            if (TextUtils.isEmpty(input)) {
+                throw new IllegalArgumentException();
+            } else {
+                MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+                messageDigest.update(input.getBytes("UTF-8"));
+                return messageDigest.digest();
+            }
+        } catch (IllegalArgumentException | NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+
+
+    /**
+     * 生成给定文件的MD5值
+     *
+     * @param path 文件路径
+     * @return
+     */
+    public static byte[] generateFileMD5(final String path) {
+        DigestInputStream dis = null;
+        try {
+            if (TextUtils.isEmpty(path)) {
+                throw new IllegalArgumentException();
+            } else {
+                FileInputStream fis = new FileInputStream(path);
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                dis = new DigestInputStream(fis, md);
+                final byte[] buffer = new byte[1024];
+                int read;
+                do {
+                    read = dis.read(buffer, 0, buffer.length);
+                } while (read != -1);
+                return md.digest();
+            }
+        } catch (IllegalArgumentException | NoSuchAlgorithmException | IOException e) {
+            return null;
+        } finally {
+            try {
+                if (dis != null) {
+                    dis.close();
+                }
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+
+}
